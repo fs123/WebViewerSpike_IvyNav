878509c3e6db0bb553f6368be30165bb27b06beb
oustriem <marco.buri@gmail.com>
2015-06-15 15:49:17 +0200

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,53 @@
 /**
  * Created by Marco on 05.05.2015.
  */
+'use strict';
 
 module.exports = function (grunt) {
 
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-karma');
+
+    // the above (uncommented) tasks can be put together if load-grunt-tasks is installed:
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
+
         config: {
             dist: 'C:/Users/Marco/development/htdocs/<%= pkg.name %>'
         },
+
+        clean: ['./output/'],
+
+        browserify: {
+            options: {
+                watch: false,
+                browserifyOptions: {
+                    debug:true // include source maps.
+                },
+                transform: [ 'brfs' ]
+            },
+            dev: {
+                expand: true,
+                src: ['./lib/**/*.js', './demo/**/*.js'],
+                dest: './output/',
+                options: {
+                    keepAlive: false
+                }
+            },
+            package: {
+                expand: true,
+                src: ['./lib/**/*.js', './demo/**/*.js'],
+                dest: './output/',
+                options: {
+                    keepAlive: false
+                }
+            }
+        },
+
         copy: {
             main: {
                 files: [
@@ -20,17 +59,26 @@ module.exports = function (grunt) {
                     {expand: true, src: ['resources/*'], dest: '<%= config.dist %>'}
                 ]
             },
+            demo: {
+                files: [
+                    // includes files within path
+                    {expand: true, src: ['resources/**/*'], dest: 'output/'},
+                    {expand: true, src: ['demo/**/*', '!**/*.js'], dest: 'output/'}
+                ]
+            },
             gugus: {
                 src: 'demo/*',
                 dest: 'dest/'
             }
         },
+
         watch: {
             main: {
                 files: ['demo/*'],
                 tasks: ['copy:gugus']
             }
         },
+
         karma: {
             single: {
                 singleRun: true,
@@ -43,9 +91,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.loadNpmTasks('grunt-karma');
+    grunt.registerTask('build', ['clean', 'browserify:package', 'copy:demo']);
 };
